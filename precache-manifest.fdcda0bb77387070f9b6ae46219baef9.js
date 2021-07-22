6e2742e9ee82cec59eed4ed41a9d308d24f459b6
Code Sandbox <hello@codesandbox.io>
2021-07-22 07:09:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fdcda0bb77387070f9b6ae46219baef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fa3158d0679a39e0fba",
+    "url": "https://munkgorn.github.io/csb-gxrlw/static/css/main.72da2ce5.chunk.css"
+  },
+  {
+    "revision": "5fa3158d0679a39e0fba",
+    "url": "https://munkgorn.github.io/csb-gxrlw/static/js/main.5fa3158d.chunk.js"
+  },
+  {
+    "revision": "d77e9515f6e4350b36e0",
+    "url": "https://munkgorn.github.io/csb-gxrlw/static/css/1.7f59b072.chunk.css"
+  },
+  {
+    "revision": "d77e9515f6e4350b36e0",
+    "url": "https://munkgorn.github.io/csb-gxrlw/static/js/1.d77e9515.chunk.js"
+  },
+  {
+    "revision": "706a4908abf03b97fae4",
+    "url": "https://munkgorn.github.io/csb-gxrlw/static/js/runtime~main.706a4908.js"
+  },
+  {
+    "revision": "030de173ff83ad431edba2f48d0b72cb",
+    "url": "https://munkgorn.github.io/csb-gxrlw/index.html"
+  }
+];
